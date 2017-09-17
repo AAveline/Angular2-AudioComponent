023d4083dde565e7435f42676d3b75ac1207fd82
karbonn <aymeric.aveline@gmail.com>
2017-09-17 17:32:11 +0200

    Git add common module

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
-
+import {CommonModule} from '@angular/common';
 import {AudioComponent} from './src/audio.component';
 
 export * from './src/audio.component';
 
 @NgModule({
+    imports: [
+        CommonModule
+    ],
     declarations: [
         AudioComponent
     ],
